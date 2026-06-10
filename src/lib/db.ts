87ab84428604bcb5bc432ec9a6b3a9ff7bf90aae
SoyNashi <mrnilopand@gmail.com>
2026-06-10 20:33:46 +0200

    dfb

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -6,9 +6,13 @@ const connectionString = process.env.DATABASE_URL;
 
 let db: ReturnType<typeof drizzle> | null = null;
 
-if (connectionString) {
-  const poolConnection = mysql.createPool(connectionString);
-  db = drizzle(poolConnection, { schema, mode: "default" });
+if (connectionString && connectionString.trim() !== "") {
+  try {
+    const poolConnection = mysql.createPool(connectionString);
+    db = drizzle(poolConnection, { schema, mode: "default" });
+  } catch (error) {
+    console.error("Error connecting to database:", error);
+  }
 }
 
 export { db };
